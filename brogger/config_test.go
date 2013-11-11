6e51d700cf3cc3b27e6bf63ecc54d0dcd7f4b236
Uiri <uiri00@gmail.com>
2013-11-11 15:51:12 -0500

    Add tests to config.go
    Add a test for the selfValidate method
    Add a test to make sure that going from json to the config struct and
    back to json gets the original json.

---
+++ brogger/config_test.go
@@ -0,0 +1,81 @@
+package brogger
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+var config *Config
+
+func TestSelfValidate(t *testing.T) {
+	config = newDefaultConfig()
+	err := config.selfValidate()
+	if err != nil {
+		t.Error("Error validating default conig")
+	}
+	config.ProdPortNumber = -1
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("-1 is not a valid production port number")
+	}
+	config.ProdPortNumber = 65600
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("656000 is not a valid production port number")
+	}
+	config.ProdPortNumber = DefaultProdPortNumber
+	config.DevelPortNumber = -1
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("-1 is not a valid development port number")
+	}
+	config.DevelPortNumber = 65600
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("65600 is not a valid development port number")
+	}
+	config.DevelPortNumber = DefaultDevelPortNumber
+	config.MaxCPUs = -1
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("-1 is not a valid number of threads")
+	}
+	config.MaxCPUs = DefaultMaxCPUs
+	config.PostFileExt = ""
+	err = config.selfValidate()
+	if err == nil {
+		t.Error("Post file extension cannot be empty")
+	}
+}
+
+func TestJsonConfigStruct(t *testing.T) {
+	os.Chdir("base")
+	defer os.Chdir("..")
+	config, _ = loadConfig()
+	config.persistToFile("test_config.json")
+	defer os.Remove("test_config.json")
+	origfile, _ := os.Open("brog_config.json")
+	testfile, _ := os.Open("test_config.json")
+	origbuf := make([]byte, 512)
+	testbuf := make([]byte, 512)
+	for {
+		i, origerr := origfile.Read(origbuf)
+		j, testerr := testfile.Read(testbuf)
+		if origerr == io.EOF || testerr == io.EOF {
+			if !(origerr == io.EOF && testerr == io.EOF) {
+				t.Error("One config file ended before the other")
+			}
+			break
+		}
+		if i != j {
+			t.Error("Different amounts read into each config file buffer:", i, j)
+			break
+		}
+		for k := 0; k < i; k++ {
+			if origbuf[k] != testbuf[k] {
+				t.Error("Config files are not byte-by-byte equal")
+			}
+		}
+	}
+}
